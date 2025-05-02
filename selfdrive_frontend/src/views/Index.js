71264c3648e236b1a4ee3e9cfe5332cbd6d93a14
Ubuntu <Administrator@UBUNTU-89.213.177.84>
2025-05-02 16:05:18 +0000

    Add selfdrive_frontend

---
+++ selfdrive_frontend/src/views/Index.js
@@ -0,0 +1,87 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.2.4
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2024 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import { useState } from "react";
+// node.js library that concatenates classes (strings)
+import classnames from "classnames";
+// javascript plugin for creating charts
+import Chart from "chart.js";
+// react plugin used to create charts
+import { Line, Bar } from "react-chartjs-2";
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  NavItem,
+  NavLink,
+  Nav,
+  Progress,
+  Table,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+
+// core components
+import {
+  chartOptions,
+  parseOptions,
+  chartExample1,
+  chartExample2,
+} from "variables/charts.js";
+
+import Header from "components/Headers/Header.js";
+import CameraStream from "components/CameraStream.js"; // ✅ import component ที่เราสร้าง
+
+const Index = (props) => {
+  const [activeNav, setActiveNav] = useState(1);
+  const [chartExample1Data, setChartExample1Data] = useState("data1");
+
+  if (window.Chart) {
+    parseOptions(Chart, chartOptions());
+  }
+
+  const toggleNavs = (e, index) => {
+    e.preventDefault();
+    setActiveNav(index);
+    setChartExample1Data("data" + index);
+  };
+
+  return (
+    <>
+      <Header />
+      <Container className="mt--7" fluid>
+        <Row>
+          <Col xl="12">
+            {/* สามารถเพิ่ม chart หรือ component อื่นได้ตรงนี้ */}
+          </Col>
+        </Row>
+        <Row className="mt--10">
+          <Col className="mb-5 mb-xl-0" xl="8">
+            {/* ✅ กล้อง Live จาก Pi */}
+            <CameraStream />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Index;
+
